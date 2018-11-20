aefbc8e983353ae63a4920ae64c80bbf7fe7e73b
GibinGeorge7 <gibin.g@gmail.com>
2018-11-20 11:36:21 +0400

    sync_work1 20-nov

---
+++ 0.Basic/ex_LIST3.py
@@ -0,0 +1,14 @@
+
+#LIST INDEX
+list2 = ['pat', 'bat', 'mat', 'cat', 'pet','cat']
+# Will print the first index of 'cat' in list2
+print(list2.index('cat'))
+
+
+#remove items from LIST using INDEX
+names = ['foo', 'bar', 'baz', 'moo', 'qux']
+#build list excluding index items 2:4
+names[2:4] = []
+print(names)
+
+
